b9ab2cf5a00040c5a0b3f9ed2a78855589f6b08a
binarybinayak <binayaksadhukhan@gmail.com>
2026-02-22 02:15:55 +0530

    Refactor app.ts to dynamically serve React app and handle missing client dist

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import apiRouter from "./api.routes";
 import path from "path";
+import fs from "fs";
 
 const app = express();
 
@@ -10,8 +11,29 @@ app.use("/sounds", express.static(path.join(__dirname, "../sounds")));
 // API routes
 app.use("/api", apiRouter);
 
-// Serve React app from apps/client/dist (assuming Vite build)
-const clientDistPath = path.join(__dirname, "../../client/dist");
-app.use(express.static(clientDistPath));
+const packageJsonInParent = path.resolve(__dirname, "../package.json");
+const serverRootPath = fs.existsSync(packageJsonInParent)
+  ? path.resolve(__dirname, "..")
+  : path.resolve(__dirname, "../..");
+
+const clientDistPath = path.resolve(serverRootPath, "../client/dist");
+
+if (fs.existsSync(clientDistPath)) {
+  app.use(express.static(clientDistPath));
+
+  app.get("/{*splat}", (req: Request, res: Response, next) => {
+    if (req.path.startsWith("/api") || req.path.startsWith("/sounds")) {
+      return next();
+    }
+
+    res.sendFile(path.join(clientDistPath, "index.html"));
+  });
+} else {
+  app.get("/", (_req: Request, res: Response) => {
+    res.status(200).json({
+      message: "There is some issue, contact admin.",
+    });
+  });
+}
 
 export default app;
